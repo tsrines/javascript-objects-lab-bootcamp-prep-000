4fdd301694390a1ac6ebcd6a5e976e79a2fb1d8c
Timothy Rines <tsrines@gmail.com>
2019-08-09 05:30:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function updateObjectWithKeyAndValue(object , key , value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value
-  console.log(object)
+  object[key] = value
+  
   
   return object
 }
